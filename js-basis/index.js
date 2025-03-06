b4dfd96556a37de9b0bbd663e947f54ee18ca996
fidel-stack <muendofidelis2@gmail.com>
2025-03-06 12:48:20 +0300

    top5emojis

---
+++ js-basis/index.js
@@ -0,0 +1,13 @@
+        //My first javascript code!
+// const person = {
+//         'firstName': 'Bruce',
+//         'lastName': 'Wayne',
+//         'Age': '25'
+
+// }
+// console.log(person.firstName)
+// oddNumbers = [1, 3, 5, 7, 9]
+// console.log(oddNumbers[0])
+console.log(true + '3')
+console.log('4' - '3')
+console.log(5 + undefined)
